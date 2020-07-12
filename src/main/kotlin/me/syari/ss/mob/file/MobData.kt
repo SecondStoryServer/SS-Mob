570631695079cf6ae823f9709d74cf0935c1c83e
sya-ri <sya79lua@gmail.com>
2020-07-13 00:10:40 +0900

    skillをグループに分けることに成功

--- src/main/kotlin/me/syari/ss/mob/file/MobData.kt
+++ src/main/kotlin/me/syari/ss/mob/file/MobData.kt
@@ -3,6 +3,7 @@ package me.syari.ss.mob.file
 import me.syari.ss.core.Main.Companion.console
 import me.syari.ss.core.config.CreateConfig.config
 import me.syari.ss.mob.Main.Companion.mobPlugin
+import me.syari.ss.mob.file.exception.SkillFormatException
 import java.io.File
 import java.io.StringReader
 
@@ -21,7 +22,57 @@ class MobData private constructor(
         }
         val skillLines = skillContent?.let { content ->
             val lines = content.withoutComment.withoutBlankLines.withIndentWidth
-            lines
+            val statementGroup = StatementGroup.SubGroup(null, "")
+            val minIndentWidth = lines.firstOrNull()?.second
+            if (minIndentWidth != null) {
+                var lastDepth = 1
+                var currentGroup = statementGroup
+                lines.forEach { (statement, indentWidth) ->
+                    val depth = indentWidth / minIndentWidth
+                    if (depth < lastDepth) {
+                        for (i in depth until lastDepth) {
+                            currentGroup.parentGroup?.let {
+                                currentGroup = it
+                            } ?: throw SkillFormatException("Unexpected Indent '$statement'")
+                        }
+                        lastDepth = depth
+                    }
+                    if (":\\s*\$".toRegex().find(statement) != null) {
+                        if (lastDepth <= depth) {
+                            currentGroup = currentGroup.addSubGroup(currentGroup, statement)
+                            lastDepth++
+                        } else {
+                            println("Depth Error $lastDepth->$depth '$statement'")
+                        }
+                    } else {
+                        currentGroup.addStatement(statement)
+                    }
+                }
+            }
+
+            buildString {
+                fun groupToString(
+                    statementGroup: StatementGroup,
+                    depth: Int
+                ) {
+                    statementGroup.get().forEach {
+                        when (it) {
+                            is StatementGroup.Statement -> {
+                                for (i in 0 until depth) {
+                                    append("  ")
+                                }
+                                appendln(it.statement)
+                            }
+                            is StatementGroup.SubGroup -> {
+                                appendln(it.statement)
+                                groupToString(it, depth + 1)
+                            }
+                        }
+                    }
+                }
+
+                groupToString(statementGroup, 0)
+            }
         }
         mobPlugin.logger.info(
             """
@@ -58,5 +109,28 @@ class MobData private constructor(
         private inline val List<String>.withIndentWidth get() = map { it to it.indentWidth }
 
         private inline val String.indentWidth get() = indexOfFirst { !it.isWhitespace() }
+
+        private sealed class StatementGroup {
+            private val content = mutableListOf<StatementGroup>()
+
+            fun addStatement(statement: String) {
+                content.add(Statement(statement))
+            }
+
+            fun addSubGroup(
+                parentGroup: SubGroup?,
+                statement: String
+            ): SubGroup {
+                return SubGroup(parentGroup, statement).apply { content.add(this) }
+            }
+
+            fun get() = content.toList()
+
+            data class Statement(val statement: String): StatementGroup()
+            data class SubGroup(
+                val parentGroup: SubGroup?,
+                val statement: String
+            ): StatementGroup()
+        }
     }
 }
---
+++ src/main/kotlin/me/syari/ss/mob/file/exception/SkillFormatException.kt
@@ -0,0 +1,3 @@
+package me.syari.ss.mob.file.exception
+
+class SkillFormatException(message: String) : Exception(message)
